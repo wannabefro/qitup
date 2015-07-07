54a696b459e86e066ffc67b445666e3b25a7cabc
wannabefro <sam.mctaggart@gmail.com>
2015-07-07 20:48:44 +0100

    initial commit

---
+++ frontend/app/adapters/application.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+import $ from 'jquery';
+
+export default DS.ActiveModelAdapter.extend({
+  headers: {
+    "X-CSRF-Token": $('meta[name="csrf-token"]').attr('content')
+  }
+});
