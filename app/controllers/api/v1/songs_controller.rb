2c6efe4a6755f4c2e539ac55b4b0d666c8c43816
wannabefro <sam.mctaggart@gmail.com>
2015-07-08 22:18:54 +0100

    index view for all songs

--- app/controllers/api/v1/songs_controller.rb
+++ app/controllers/api/v1/songs_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::SongsController < ApplicationController
 
+  def index
+    songs = Song.all
+    render json: songs
+  end
+
   def show
     song = Song.find(params[:id])
     render json: song
--- frontend/app/router.js
+++ frontend/app/router.js
@@ -9,10 +9,10 @@ Router.map(function() {
   this.route('dashboard');
   this.route('sets');
   this.route('songs', function() {
+    this.route('index', { path: '/' });
     this.route('new');
     this.route('show', { path: '/:song_id' });
   });
-
 });
 
 export default Router;
